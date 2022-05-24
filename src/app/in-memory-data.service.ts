74148e538ae57bbba7e6ba5cf1cd8e7c18878c92
Brando-Sosa <l16390421@chetumal.tecnm.mx>
2022-05-24 12:31:42 -0500

    29 files modified

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -22,19 +22,10 @@ export class InMemoryDataService implements InMemoryDbService {
     return {heroes};
   }
 
-  // Overrides the genId method to ensure that a hero always has an id.
-  // If the heroes array is empty,
-  // the method below returns the initial number (11).
-  // if the heroes array is not empty, the method below returns the highest
-  // hero id + 1.
+ 
   genId(heroes: Hero[]): number {
     return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
   }
 }
 
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -14,8 +14,3 @@ export const HEROES: Hero[] = [
 ];
 
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
